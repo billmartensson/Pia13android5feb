b844c16e0c4e9a390db9b8a585a18551b83cc503
Bill Mårtensson <bill@magictechnology.se>
2025-02-05 13:37:21 +0100

    pia13

--- app/src/main/java/se/magictechnology/pia13android5feb/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia13android5feb/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,6 +26,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,6 +52,8 @@ fun Mathscreen() {
 
     var mathtotal by remember { mutableStateOf(0) }
     var usernumber by remember { mutableStateOf("") }
+
+    var usererror by remember { mutableStateOf("") }
 
     fun docalc(calctype : String) {
 
@@ -74,48 +85,101 @@ fun Mathscreen() {
         }
 
         addnumber?.let {
+            usererror = ""
+
             if(calctype == "plus") {
                 mathtotal = mathtotal + it
             }
             if(calctype == "minus") {
                 mathtotal = mathtotal - it
             }
+            if(calctype == "mult") {
+                mathtotal = mathtotal * it
+            }
+            if(calctype == "div") {
+                if(it == 0) {
+                    usererror = "You cannot divide by zero"
+                } else {
+                    mathtotal = mathtotal / it
+                }
 
+            }
+        }
+
+        if(addnumber == null) {
+            usererror = "You entered bad number"
         }
 
     }
 
-
-
-
-
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("$mathtotal", fontSize = 70.sp)
 
-        TextField(
+        Text("$mathtotal",
+            fontSize = 100.sp,
+            modifier = Modifier.padding(vertical = 20.dp)
+        )
+
+        if(usererror != "") {
+            Text(usererror,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .height(50.dp)
+                    .fillMaxWidth()
+                    .background(Color.Red)
+                    .padding(5.dp)
+                    .clickable {
+                        usererror = ""
+                    }
+            )
+        }
+
+        OutlinedTextField(
             value = usernumber,
             onValueChange = {
+                /*
                 if(it.toIntOrNull() != null || it == "") {
                     usernumber = it
                 }
+                 */
+                usernumber = it
             },
             label = { Text("Enter a number") },
             modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp)
         )
 
-        Button(onClick = {
-            docalc("plus")
-        }) {
-            Text("PLUS")
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp)) {
+            Button(onClick = {
+                docalc("plus")
+            }) {
+                Text("PLUS")
+            }
+
+            Button(onClick = {
+                docalc(calctype = "minus")
+            }) {
+                Text("MINUS")
+            }
         }
 
-        Button(onClick = {
-            docalc(calctype = "minus")
-        }) {
-            Text("MINUS")
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp)) {
+            Button(onClick = {
+                docalc("mult")
+            }) {
+                Text("MULT")
+            }
+
+            Button(onClick = {
+                docalc(calctype = "div")
+            }) {
+                Text("DIV")
+            }
         }
 
-        Button(onClick = {
+        Column(modifier = Modifier.weight(1F)) {  }
+
+        Button(modifier = Modifier.padding(bottom = 20.dp), onClick = {
             mathtotal = 0
         }) {
             Text("RESET")
